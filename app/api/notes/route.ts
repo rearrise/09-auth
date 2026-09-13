7a470ec77c57a96acd3b2cf7ccd424c0e89d2b93
Danylo <84040699+rearrise@users.noreply.github.com>
2026-09-13 20:10:23 +0300

    fix path to api

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { api } from "../../../lib/api/api";
+import { api } from "../../api/api";
 import { cookies } from "next/headers";
 import { isAxiosError } from "axios";
 import { logErrorResponse } from "../_utils/utils";
